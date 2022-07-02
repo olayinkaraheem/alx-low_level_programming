386996dc02b795b874c649e0c45ab85714303659
Kareem Raheem <olayinka.raheem16@gmail.com>
2022-07-01 20:35:00 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar%s but the outcome is a piece of art%s", ",", ",");
+	return (0);
+}
