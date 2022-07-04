c45de3ff879aa5e5a236bf8d8283e9dbfe155e3c
Kareem Raheem <olayinka.raheem16@gmail.com>
2022-07-04 06:01:10 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lastChar;
+
+	for (lastChar = 'a'; lastChar <= 'z'; lastChar++)
+	{
+		if (lastChar != 'e' && lastChar != 'q')
+			putchar(lastChar);
+	}
+	putchar('\n');
+	return (0);
+}
