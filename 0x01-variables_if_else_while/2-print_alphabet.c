0130f7ef70891642bfec261c010b22c9ed82ef35
Kareem Raheem <olayinka.raheem16@gmail.com>
2022-07-03 17:23:43 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	for(char lastChar = 'a'; lastChar < 'z'; lastChar++)
+{
+	char lastChar;
+
+	for(lastChar = 'a'; lastChar <= 'z'; lastChar++)
 	{
 		putchar(lastChar);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
