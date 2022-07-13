22ce0910b42b7a673d65052062cbec0c43bc6467
Kareem Raheem <olayinka.raheem16@gmail.com>
2022-07-13 14:07:19 -0700

    1. I sometimes suffer from insomnia. And when...

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-#include <main.h>
+#include "main.h"
 
 /**
  * print_alphabet - prints all alphabets in lowercase
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * Main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
