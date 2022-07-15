e3380ee23ce304c1b7428d1bd8d3221c09ae9f62
Kareem Raheem <olayinka.raheem16@gmail.com>
2022-07-15 00:43:32 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _islower - checks if a character is lowercase or uppercase.
+ * @letter: the character to check.
+ *
+ * returns 1 if uppercase, 0 if lowercase
+ *
+ * Return: int.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _islower(char letter)
+{
+	if ((int)letter >= 97 && (int)letter <= 122)
+		return (0);
+	else
+		return (1);
+}
