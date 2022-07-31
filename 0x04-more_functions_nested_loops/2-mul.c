f6521a6d45c59c6f4e9b4bb9c4d2eb7a125670d2
Kareem Raheem <olayinka.raheem16@gmail.com>
2022-07-31 00:54:04 -0700

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is a digit or not.
+ * @a: the first integer.
+ * @b: the second integer
+ *
+ * returns result of a * b
+ *
+ * Return: int.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
