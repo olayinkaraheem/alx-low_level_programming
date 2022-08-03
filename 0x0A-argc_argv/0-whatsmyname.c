724c944542c2f31087bc4f761ee1419f2a37eaaa
Kareem Raheem <olayinka.raheem16@gmail.com>
2022-08-03 14:02:50 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * @argc: argument count
+ * @argv: argument vector. A list of arguments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
