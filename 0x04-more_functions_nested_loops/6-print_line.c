9783a49c19d12b5f8dcbcdca544e565248227be4
Kareem Raheem <olayinka.raheem16@gmail.com>
2022-08-15 06:23:51 -0700

    6. The shortest distance between two points is a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,16 +14,13 @@ void print_line(int n)
 {
 	int num = 0;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-		return;
-	}
-
-	while (num <= n)
-	{
-		_putchar('_');
-		num++;
+		while (num <= n)
+		{
+			_putchar('_');
+			num++;
+		}
 	}
 	_putchar('\n');
 }
